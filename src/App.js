9fba32e0c48e6d2522a9410800bda7d1d3366bef
jongyeol <pjr159@gmail.com>
2021-12-26 23:07:33 +0900

    first fetch data with react-query

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import './App.css'
 import { HomePage } from './components/Home.page'
 import { RQtodos } from './components/RQtodos.page'
 import { Todos } from './components/Todos.page'
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/todos'>Traditional todos</Link>
-            </li>
-            <li>
-              <Link to='/rq-todos'>RQ todos</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path='/todos'>
-            <Todos />
-          </Route>
-          <Route path='/rq-todos'>
-            <RQtodos />
-          </Route>
-          <Route path='/'>
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/todos'>Traditional todos</Link>
+              </li>
+              <li>
+                <Link to='/rq-todos'>RQ todos</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/todos'>
+              <Todos />
+            </Route>
+            <Route path='/rq-todos'>
+              <RQtodos />
+            </Route>
+            <Route path='/'>
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </QueryClientProvider>
   )
 }
 
--- src/components/RQtodos.page.js
+++ src/components/RQtodos.page.js
@@ -1,3 +1,24 @@
+import { useQuery } from "react-query";
+import axios from 'axios';
+
+const fetchTodos = () => {
+    return axios.get('http://localhost:4000/todos');
+}
+
 export const RQtodos = () => {
-    return <div>RQ tods\os</div>
+    const { isLoading, data } = useQuery('todos', fetchTodos);
+
+    if (isLoading) {
+        return <h2>Loading...!!</h2>
+    }
+
+    return (
+        <>
+            {
+                data?.data.map(cv => {
+                    return <div key={cv.name}>{cv.name}</div>
+                })
+            }
+        </>
+    )
 }
