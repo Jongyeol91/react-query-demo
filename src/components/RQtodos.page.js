e2974634e8d73a5af237bb588c3619ee8962c2b2
jongyeol <pjr159@gmail.com>
2021-12-26 23:30:15 +0900

    handling query error

--- src/components/RQtodos.page.js
+++ src/components/RQtodos.page.js
@@ -2,14 +2,19 @@ import { useQuery } from "react-query";
 import axios from 'axios';
 
 const fetchTodos = () => {
-    return axios.get('http://localhost:4000/todos');
+    return axios.get('http://localhost:4000/todoss');
 }
 
 export const RQtodos = () => {
-    const { isLoading, data } = useQuery('todos', fetchTodos);
+    const { isLoading, data, isError, error } = useQuery('todos', fetchTodos);
 
     if (isLoading) {
         return <h2>Loading...!!</h2>
+    }
+
+
+    if (isError) {
+        return <h2>{error.message}</h2>
     }
 
     return (
--- src/components/Todos.page.js
+++ src/components/Todos.page.js
@@ -5,16 +5,24 @@ import axios from 'axios';
 export const Todos = () => {
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [error, setError] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:4000/todos').then((res) => {
+        axios.get('http://localhost:4000/todoss').then((res) => {
             setData(res.data);
             setLoading(false)
+        }).catch((error) => {
+            setError(error.message);
+            setLoading(false);
         })
     }, [])
 
     if (loading) {
         return <h2>loading</h2>
+    } 
+
+    if (error) {
+        return <h2>{error}</h2>
     }
 
     return(
