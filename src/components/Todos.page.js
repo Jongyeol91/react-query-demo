9e7e9a062a6ee1f8b72e2390e4c827d031fecc2d
jongyeol <pjr159@gmail.com>
2021-12-26 22:47:17 +0900

    setup fetch data by traditional way

--- src/components/Todos.page.js
+++ src/components/Todos.page.js
@@ -1,3 +1,27 @@
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export const Todos = () => {
-    return <div>todos</div>
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/todos').then((res) => {
+            setData(res.data);
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return <h2>loading</h2>
+    }
+
+    return(
+        <>
+            {data.map(cv => {
+                return <div key={cv.name}>{cv.name}</div>
+            })}
+        </>
+    )
 }
